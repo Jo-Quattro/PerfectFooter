7a4d373f83e5b33c85dffa41aa7d6d55765fd746
Nicolas GIRARD <NG38@outlook.fr>
2025-04-08 10:56:23 +0200

    ajout d'un mailto sur le bouton contact

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,17 +3,18 @@ import Contact from "./Contact.tsx";
 
 function Nav(){
 
-
+    const openMailApp = () => {
+        window.location.href = "mailto:contact@example.com"; // Remplace par l'adresse e-mail souhaitée
+    };
     return (
         <nav className="nav">
             <ul>
                 <li>Home</li>
                 <li>Gallery</li>
                 <li>About</li>
-                <li onClick={<Contact />}>Contact</li>
+                <li onClick={openMailApp}>Contact</li>
             </ul>
         </nav>
-
     )
 }
 
